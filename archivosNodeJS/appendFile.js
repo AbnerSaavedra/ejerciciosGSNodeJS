6684ea586cbac3c64c4ecf0f7337ec166ebaf47e
Abner Saavedra <ingenieroabnersaavedra@gmail.com>
2024-08-20 09:53:04 -0400

    Práctica de sistema de archivos

--- archivosNodeJS/appendFile.js
+++ archivosNodeJS/appendFile.js
@@ -2,11 +2,11 @@ const { appendFile} = require('fs/promises');
 
 async function appendToFile(fileName, data) {
     try {
-    await appendFile(fileName, data, { flag: 'w' });
+    await appendFile(fileName, data, { flag: 'a' });
     console.log(`Appended data to ${fileName}`);
     } catch (error) {
     console.error(`Got an error trying to append the
-    file: {error.message}`);
+    file: ${error.message}`);
     }
 }
 
--- fileManager.js
+++ fileManager.js
@@ -1,9 +1,12 @@
 const rfile = require("./archivosNodeJS/readFile")
 const wfile = require("./archivosNodeJS/writeFile")
 const afile = require("./archivosNodeJS/appendFile")
+const ofile = require("./archivosNodeJS/openFile")
 
 rfile.readThisFile("./files/participantes.txt")
 
-wfile.writeToFile("./files/fileToWrite.txt", "Hola mundo")
+wfile.writeToFile("./files/fileToWrite-1.txt", "Hola mundo 2")
 
-afile.appendToFile("./files/fileToAppend.txt", "Hola mundo one more time")
+afile.appendToFile("./files/fileToAppend.txt", "Hola mundo")
+
+ofile.openFile("./files/fileToOpen.txt", "Hola mundo 7")
