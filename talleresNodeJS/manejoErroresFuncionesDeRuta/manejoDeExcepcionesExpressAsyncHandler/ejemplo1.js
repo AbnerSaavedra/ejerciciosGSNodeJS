074b188453277537b05af82ef10c8abec9ce6d47
Abner Saavedra <ingenieroabnersaavedra@bitbucket.org>
2024-09-04 15:30:59 -0400

    Manejo de excepciones con Express async handler

--- talleresNodeJS/manejoErroresFuncionesDeRuta/manejoDeExcepcionesExpressAsyncHandler/ejemplo1.js
+++ talleresNodeJS/manejoErroresFuncionesDeRuta/manejoDeExcepcionesExpressAsyncHandler/ejemplo1.js
@@ -4,11 +4,13 @@ const path = require('path')
 const router = express.Router()
 const asyncHandler = require('express-async-handler')
 
-router.get("/users", asyncHandler( (req, res, next =>{
-    const successfulResult = fs.readFileSync(path.join(__dirname, 'estudiantes.json'))
+router.get("/students", asyncHandler( async (req, res, next) =>{
+
+    const successfulResult = await fs.readFileSync(path.join(__dirname, 'estudiantes.json'))
     res.writeHead(200, {'Content-Type': 'text/json'});
-    res.end(JSON.parse(successfulResult));
-})
-)) 
+    res.end(successfulResult);
+    })
+)
+
 
 module.exports = router
