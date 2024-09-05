8e1320d9f20da24f55ef12707597cf2dcfe78eac
Abner Saavedra <ingenieroabnersaavedra@gmail.com>
2024-09-05 09:49:49 -0400

    Rutas anidadas

---
+++ talleresNodeJS/rutas&Controladores/proyectoControladoresRutasAnidadas/routes/index.js
@@ -0,0 +1,10 @@
+// routes/index.js 
+const express = require('express'); 
+const router = express.Router(); 
+const userRoutes = require('./userRoutes');
+//const postRoutes = require('./postRoutes')
+
+router.use('/user', userRoutes);
+//router.use('/post', postRoutes);
+
+module.exports = router;
